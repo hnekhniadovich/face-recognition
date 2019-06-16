f41e3dae4c1ec45039f950bee2e6031f46fb2c6f
hnekhniadovich <nekhanya@gmail.com>
2019-06-16 18:40:02 -0400

    create border around the box

--- src/App.js
+++ src/App.js
@@ -29,12 +29,32 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value }); 
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
   }
 
   onButtonSubmit = () => {
@@ -45,11 +65,8 @@ class App extends Component {
           // URL
           this.state.input
       )
-      .then(function(response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-          },
-          function(err) {}// there was an error}
-      );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))// there was an error}
   }
 
   render() {
@@ -61,7 +78,7 @@ class App extends Component {
           <Logo />
           <Rank />
           <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition imageUrl={this.state.imageUrl}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
   
       </div>
     );
