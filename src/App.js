5ed148060ebef31dffda680867b35b311cbb9fda
hnekhniadovich <nekhanya@gmail.com>
2019-06-16 01:26:52 -0400

    add rank Component, particles plugin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <Particles className="particles"
+        params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
 
     </div>
   );
